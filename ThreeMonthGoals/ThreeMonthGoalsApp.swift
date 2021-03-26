8397bde27c2f5d0e113ceeccc8e8653a4fef65ba
Tyler Moudy <tyler.moudy@gmail.com>
2021-03-25 20:07:30 -0500

    Initial Commit

---
+++ ThreeMonthGoals/ThreeMonthGoalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreeMonthGoalsApp.swift
+//  ThreeMonthGoals
+//
+//  Created by Tyler Moudy on 3/25/21.
+//
+
+import SwiftUI
+
+@main
+struct ThreeMonthGoalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
